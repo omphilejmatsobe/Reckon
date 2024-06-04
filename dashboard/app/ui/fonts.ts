0255479d1ad354dcab11a989425cc8da44cd7055
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-06-04 12:29:20 +0200

    Tailwind styles update

---
+++ dashboard/app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from 'next/font/google'
+
+export const interFont = Inter({ subsets:['latin'] });
+export const lusitanaFont = Lusitana({ weight:['400', '700'], subsets:['latin'] });
