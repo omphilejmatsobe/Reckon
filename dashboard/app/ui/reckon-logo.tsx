ac1d7fcc45509a91470b116653044fabaaaec37d
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-06-04 16:53:40 +0200

    dashboard Image added

--- dashboard/app/ui/reckon-logo.tsx
+++ dashboard/app/ui/reckon-logo.tsx
@@ -1,13 +1,13 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitanaFont } from '@/app/ui/fonts';
 
-export default function InsightLogo() {
+export default function ReckonLogo() {
   return (
     <div
       className={`${lusitanaFont.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Insight</p>
+      <p className="text-[44px]">reck·on</p>
     </div>
   );
 }
