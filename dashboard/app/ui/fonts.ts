432c722abd95b533e597afdbc69a986a984edeee
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-06-05 04:43:29 +0200

    App update

--- dashboard/app/ui/fonts.ts
+++ dashboard/app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import { Inter, Lusitana } from 'next/font/google'
 
 export const interFont = Inter({ subsets:['latin'] });
-export const lusitanaFont = Lusitana({ weight:['400', '700'], subsets:['latin'] });
+export const lusitanaFont = Lusitana({ weight:['400', '700'], subsets:['lusitana'] });
