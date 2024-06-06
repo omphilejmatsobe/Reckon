f7b74448693bfd6fe397ffb2f18648b1ddc21b0a
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-06-07 00:12:59 +0200

    Suspense added

--- dashboard/app/dashboard/(overview)/page.tsx
+++ dashboard/app/dashboard/(overview)/page.tsx
@@ -3,6 +3,9 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitanaFont } from '@/app/ui/fonts';
 import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+
  
 export default async function Page() {
 
@@ -30,7 +33,9 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
