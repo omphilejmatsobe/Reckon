6a7309f1be6b68df852fade17d37834bb7b9d272
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-06-07 01:54:14 +0200

    Bug fix. Syntax error

--- dashboard/app/dashboard/(overview)/page.tsx
+++ dashboard/app/dashboard/(overview)/page.tsx
@@ -22,7 +22,7 @@ export default async function Page() {
           <RevenueChart />
         </Suspense>
 
-        <Suspense fallback={<LatestInvoiceSkeleton />}>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
           <LatestInvoices />
         </Suspense>
 
