f5275ed086f9d71f7385a83175175237b21a1fe5
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-06-06 16:17:40 +0200

    Bug fix

--- dashboard/app/dashboard/page.tsx
+++ dashboard/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitanaFont } from '@/app/ui/fonts';
 import { fetchRevenue } from '@/app/lib/data';
  
-export default async function Page() {i
+export default async function Page() {
 
   const revenue = await fetchRevenue();
   return (
