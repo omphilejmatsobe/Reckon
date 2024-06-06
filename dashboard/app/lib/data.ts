5b165e9b41c981c7ade5f61097bd53c108962179
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-06-06 22:22:01 +0200

    File check

--- dashboard/app/lib/data.ts
+++ dashboard/app/lib/data.ts
@@ -12,7 +12,6 @@ import { formatCurrency } from './utils';
 import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
-  noStore();
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
@@ -20,12 +19,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-     console.log('Fetching revenue data...');
-     await new Promise((resolve) => setTimeout(resolve, 3000));
+     //console.log('Fetching revenue data...');
+     //await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-     console.log('Data fetch completed after 3 seconds.');
+    // console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -35,7 +34,6 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
-  noStore();
 
   try {
     const data = await sql<LatestInvoiceRaw>`
@@ -57,7 +55,6 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
-  noStore();
 
   try {
     // You can probably combine these into a single SQL query
@@ -98,7 +95,6 @@ export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
 ) {
-  noStore();
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
@@ -131,7 +127,6 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
-  noStore();
 
   try {
     const count = await sql`SELECT COUNT(*)
@@ -198,7 +193,7 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
-  noStore();
+
   try {
     const data = await sql<CustomersTableType>`
 		SELECT
