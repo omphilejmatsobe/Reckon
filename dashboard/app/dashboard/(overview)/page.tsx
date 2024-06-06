39edffac07dfa0b69fb108f68b167ac133951066
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-06-06 23:58:44 +0200

    Stream Specific rendering implemented. Check main page.tsx

--- dashboard/app/dashboard/(overview)/page.tsx
+++ dashboard/app/dashboard/(overview)/page.tsx
@@ -2,10 +2,10 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitanaFont } from '@/app/ui/fonts';
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
  
 export default async function Page() {
-  const revenue = await fetchRevenue();
+
   const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfInvoices,
