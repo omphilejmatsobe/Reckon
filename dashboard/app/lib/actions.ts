c91df53d109caf328f3b41fbb5e933c676549b31
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-06-08 00:07:22 +0200

    App update

--- dashboard/app/lib/actions.ts
+++ dashboard/app/lib/actions.ts
@@ -1,4 +1,15 @@
 'use server';
+import { z } from 'zod';
+ 
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+ 
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
   const rawFormData = {
