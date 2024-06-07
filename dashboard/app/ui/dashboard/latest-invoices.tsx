fbf56c77c75bf13dc391bc1cf9d47ae97fdc972f
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-06-07 02:02:55 +0200

    Fix

--- dashboard/app/ui/dashboard/latest-invoices.tsx
+++ dashboard/app/ui/dashboard/latest-invoices.tsx
@@ -6,7 +6,7 @@ import { fetchLatestInvoices } from '@/app/lib/data';
 
 export default async function LatestInvoices(){
 
-  const latestInvoice = await fetchLatestInvoices();
+  const latestInvoices = await fetchLatestInvoices();
  
   return (
     <div className="flex w-full flex-col md:col-span-4">
