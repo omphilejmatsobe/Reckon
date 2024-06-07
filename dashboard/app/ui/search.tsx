30b61d7b7651f8aaee45534300b3425752369466
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-06-07 18:26:15 +0200

    Search URL implemented

--- dashboard/app/ui/search.tsx
+++ dashboard/app/ui/search.tsx
@@ -1,13 +1,22 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    }
+    else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
   }
 
   return (
