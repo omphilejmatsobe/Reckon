0e0d5faac683c30bf44367d878341cc3d92001b3
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-06-07 18:19:58 +0200

    Input params added

--- dashboard/app/ui/search.tsx
+++ dashboard/app/ui/search.tsx
@@ -1,13 +1,14 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+
   function handleSearch(term: string) {
-    console.log(term);
+    const params = new URLSearchParams(searchParams);
   }
-
-
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
