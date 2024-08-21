371a081d8b06c5c1d217a5f79f721e5a6c9b215a
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-08-21 08:05:54 +0200

    App update

--- my-app/app/seed/route.ts
+++ my-app/app/seed/route.ts
@@ -1,4 +1,4 @@
-// import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt';
 import { db } from '@vercel/postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
