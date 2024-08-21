93d4864af1d15bec01e6494195c7d097c3448f81
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-08-21 13:08:22 +0200

    App update

--- my-app/app/ui/dashboard/revenue-chart.tsx
+++ my-app/app/ui/dashboard/revenue-chart.tsx
@@ -17,11 +17,11 @@ export default async function RevenueChart({
   const chartHeight = 350;
   // NOTE: Uncomment this code in Chapter 7
 
-  // const { yAxisLabels, topLabel } = generateYAxis(revenue);
+   const { yAxisLabels, topLabel } = generateYAxis(revenue);
 
-  // if (!revenue || revenue.length === 0) {
-  //   return <p className="mt-4 text-gray-400">No data available.</p>;
-  // }
+   if (!revenue || revenue.length === 0) {
+     return <p className="mt-4 text-gray-400">No data available.</p>;
+   }
 
   return (
     <div className="w-full md:col-span-4">
