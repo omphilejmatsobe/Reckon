5c55558e94028599d673f96daff0379b18128752
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-08-25 18:54:17 +0200

    APp updated

--- my-app/app/ui/dashboard/nav-links.tsx
+++ my-app/app/ui/dashboard/nav-links.tsx
@@ -1,11 +1,11 @@
-'use client'
+'use client';
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-import { Link } from 'next/link';
-import { usePathName } from 'next/navigation';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
@@ -22,7 +22,7 @@ const links = [
 
 export default function NavLinks() {
 
-  const path = usePathName();  
+  const pathname = usePathname(); 
   return (
     <>
       {links.map((link) => {
