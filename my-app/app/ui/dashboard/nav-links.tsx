d299c0077b79114e1225dc4650714b981b0593a0
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-08-25 21:49:23 +0200

    App State Update

--- my-app/app/ui/dashboard/nav-links.tsx
+++ my-app/app/ui/dashboard/nav-links.tsx
@@ -22,21 +22,23 @@ const links = [
 
 export default function NavLinks() {
 
-  const pathname = usePathname(); 
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <Link
+	  <Link
             key={link.name}
             href={link.href}
-	    className={clsx(
+            className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
                 'bg-sky-100 text-blue-600': pathname === link.href,
               },
-            )}>
+            )}
+          >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </Link>
